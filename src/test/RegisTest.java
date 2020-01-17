cf5ed022676c92bd3fba798d175d8757428f1895
sobe-t <sobe-tadahisa@mgt-net.com>
2020-01-17 17:37:52 +0900

    こみっと

--- src/test/RegisTest.java
+++ src/test/RegisTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import dao.BasicDAO;
+import model.Account;
+
 public class RegisTest {
 
 	public static void main(String[] args) {
@@ -8,11 +11,19 @@ public class RegisTest {
 	}
 
 	private static void 登録成功のテスト() {
-
-
+		/*テストの内容
+		 * .signUpにAccountを入れて登録しする
+		 * Accountを返す
+		 * 
+		 */
+		
+		Account account = new Account("000", "minato", "minato@gmail", "pass");
+		BasicDAO dao = new BasicDAO();
+		
+		
 	}
 	private static void 登録失敗のテスト() {
-
-
+		
+		
 	}
 }
